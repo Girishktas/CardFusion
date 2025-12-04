155eb70f3d8aa4bd5d630371123f2611def4563e
Girishktas <nominnobira@gmail.com>
2025-12-04 09:56:23 +0800

    Add deployment script [skip ci]

---
+++ contracts/deploy/deploy.ts
@@ -0,0 +1,20 @@
+import { DeployFunction } from "hardhat-deploy/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+
+  const deployedCardFusion = await deploy("CardFusion", {
+    from: deployer,
+    args: ["CardFusion", "CF"],
+    log: true,
+    waitConfirmations: 1,
+  });
+
+  console.log(`CardFusion contract: `, deployedCardFusion.address);
+};
+export default func;
+func.id = "deploy_cardFusion"; // id required to prevent reexecution
+func.tags = ["CardFusion"];
+
